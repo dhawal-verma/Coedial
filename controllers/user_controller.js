59287681190e53a8635a17af41f135ea0cc2df2c
Dhawal <dhawalverma2808@gmail.com>
2022-06-23 22:09:54 +0530

    Set database and sign up page

--- controllers/user_controller.js
+++
@@ -1,4 +0,0 @@
-module.exports.profile = function(resquest,response)
-{
-    response.end('<h1>User Profile</h1>');
-}
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,13 @@ const router = express.Router();
 
 const homeController = require('../controllers/home_controller');
 
-router.get('/',homeController.home);
+console.log('router loaded');
 
-router.use('/users',require('./users'));
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
 
 module.exports = router;
